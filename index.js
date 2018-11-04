bedf979235f01a86ecedd1f5be4dcfdbfda0b119
Mary Jack <maryjackinthebox@gmail.com>
2018-11-04 23:37:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // Code your solutions in this file
- function tailsNeverFails(){
-   return Math.random() >= 0.5;
- }
- 
- let counter = 0;
- while (tailsNeverFails() <+ 0.5) {
-    counter++;
-    return 'You got ${counter} tails in a row!';
+
+
+function maybeTails() {
+  return Math.random() >= 0.5;
+}
+ function tailsNeverFails() {
+  let counter = 0;
+  while (maybeTails()) {
+    counter ++;
+  }
+  return `You got ${counter} tails in a row!`;
 }
