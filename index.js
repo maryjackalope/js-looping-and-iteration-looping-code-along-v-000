513e03ab832126c0ae8fcaf8960198b203436bff
Mary Jack <maryjackinthebox@gmail.com>
2018-11-04 23:38:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function printBadges(array) {
+  for (let i=0; i<array.length; i++) {
+    console.log(`Welcome ${array[i]}! You are employee #${i+1}.`);
+  }
+  return array;
+}
+
+function maybeTails() {
+  return Math.random() >= 0.5;
+}
+ function tailsNeverFails() {
+  let counter = 0;
+  while (maybeTails()) {
+    counter ++;
+  }
+  return `You got ${counter} tails in a row!`;
+}
